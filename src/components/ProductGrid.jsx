5bf3dbf77f33332b962bbef843e1578bcbfd18ed
Joshua Malco <malcojoshua+githubspace@gmail.com>
2026-09-22 01:07:05 +0800

    remove the title next to selected product

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -11,9 +11,9 @@ function ProductGrid() {
       if (selectedProduct) {
          console.log('Selected product: ', selectedProduct.name)
       }
-      document.title = selectedProduct
-         ? `${selectedProduct.name} - NORTHRUN`
-         : 'NORTHRUN'
+      // document.title = selectedProduct
+      //    ? `${selectedProduct.name} - NORTHRUN`
+      //    : 'NORTHRUN'
    }, [selectedProduct])
 
    const categories = [
